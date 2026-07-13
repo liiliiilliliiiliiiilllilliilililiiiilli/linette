474ea08f925502957995711bb1694959a83b3207
sapphire <lime.rainbow.li@gmail.com>
2026-07-13 23:47:15 +0300

    Обновление репозитория

--- source/web/source/src/root/root.jsx
+++ source/web/source/src/root/root.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from './app/App.jsx'
+import { App } from '../app/App.jsx'
 
-import './index.css'
+import './root.css'
 
 
 
